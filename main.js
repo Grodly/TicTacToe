b2d78ea9edacdc585c0416f4c390680aa39036cd
Grodly <Grodly@users.noreply.github.com>
2020-03-11 11:10:06 +0400

    1

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 //player1 Dark - #A62A0C Light - #e0765c
 //player2 Dark - #0c44a6 Light - #5c60e0
 
+//colors
+var DarkRed = "#A62A0C";
+var LightRed = "#e0765c";
+var DarkBlue = "#0c44a6";
+var LightBlue = "#5c60e0";
+var ErrorColor = "#ff6161";
+
+var canPlay = false;
 var turn = "X";
 var buttons = document.querySelectorAll(".btn");
 
@@ -17,21 +25,20 @@ function OnClickNewGame() {
     player2_input = document.querySelector(".player2_input");
 
     if (player1_input.value === " " && player2_input.value === " ") {
-        player1_input.style.background = "#ff6161";
-        player2_input.style.background = "#ff6161";
+        player1_input.style.background = ErrorColor;
+        player2_input.style.background = ErrorColor;
     } else if (player1_input.value === " " || player2_input.value === " ") {
         if (player1_input.value === " ") {
-            player1_input.style.background = "#ff6161";
+            player1_input.style.background = ErrorColor;
             player2_input.style.background = "#fff";
         }
         if (player2_input.value === " ") {
             player1_input.style.background = "#fff";
-            player2_input.style.background = "#ff6161";
+            player2_input.style.background = ErrorColor;
         }
     } else if (player1_input.value === player2_input.value) {
-        player1_input.style.background = "#ff6161";
-        player2_input.style.background = "#ff6161";
-        console.log("saxelebi emtxveva");
+        player1_input.style.background = ErrorColor;
+        player2_input.style.background = ErrorColor;
     } else {
         player1_input.style.background = "#fff";
         player2_input.style.background = "#fff";
@@ -39,35 +46,37 @@ function OnClickNewGame() {
         player2.textContent = player2_input.value;
 
         turn = "X";
-        player1.style.background = "#A62A0C"; //dark
-        player2.style.background = "#5c60e0"; //light
+        player1.style.background = DarkRed;
+        player2.style.background = LightBlue;
 
         for (i = 0; i < buttons.length; i++) {
             buttons[i].value = " ";
         }
-
         stats.textContent = "";
+        canPlay = true;
+    }
+}
+
+function OnClickBlock(id) {
+    if (canPlay) {
+        var btn = document.querySelector("#" + id);
+        if (btn.value === " ") {
+            btn.value = turn;
+            ChangeTurn();
+            CheckWinner();
+        }
     }
 }
 
 function ChangeTurn() {
     if (turn === "X") {
         turn = "O";
-        player1.style.background = "#e0765c"; //light
-        player2.style.background = "#0c44a6"; //dark
+        player1.style.background = LightRed;
+        player2.style.background = DarkBlue;
     } else if (turn === "O") {
         turn = "X";
-        player1.style.background = "#A62A0C"; //dark
-        player2.style.background = "#5c60e0"; //light
-    }
-}
-
-function OnClickBlock(id) {
-    var btn = document.querySelector("#" + id);
-    if (btn.value === " ") {
-        btn.value = turn;
-        ChangeTurn();
-        CheckWinner();
+        player1.style.background = DarkRed;
+        player2.style.background = LightBlue;
     }
 }
 
@@ -76,35 +85,27 @@ function CheckWinner() {
     let winner;
 
     if (buttons[0].value === buttons[1].value && buttons[0].value === buttons[2].value && buttons[0].value !== " ") {
-        console.log("gilocav!" + buttons[0].value);
         winner = buttons[0].value;
         draw = false;
     } else if (buttons[3].value === buttons[4].value && buttons[3].value === buttons[5].value && buttons[3].value !== " ") {
-        console.log("gilocav!" + buttons[3].value);
         winner = buttons[3].value;
         draw = false;
     } else if (buttons[6].value === buttons[7].value && buttons[6].value === buttons[8].value && buttons[6].value !== " ") {
-        console.log("gilocav!" + buttons[6].value);
         winner = buttons[6].value;
         draw = false;
     } else if (buttons[0].value === buttons[3].value && buttons[0].value === buttons[6].value && buttons[0].value !== " ") {
-        console.log("gilocav!" + buttons[0].value);
         winner = buttons[0].value;
         draw = false;
     } else if (buttons[1].value === buttons[4].value && buttons[1].value === buttons[7].value && buttons[1].value !== " ") {
-        console.log("gilocav!" + buttons[1].value);
         winner = buttons[1].value;
         draw = false;
     } else if (buttons[2].value === buttons[5].value && buttons[2].value === buttons[8].value && buttons[2].value !== " ") {
-        console.log("gilocav!" + buttons[2].value);
         winner = buttons[2].value;
         draw = false;
     } else if (buttons[0].value === buttons[4].value && buttons[0].value === buttons[8].value && buttons[0].value !== " ") {
-        console.log("gilocav!" + buttons[0].value);
         winner = buttons[0].value;
         draw = false;
     } else if (buttons[2].value === buttons[4].value && buttons[2].value === buttons[6].value && buttons[2].value !== " ") {
-        console.log("gilocav!" + buttons[2].value);
         winner = buttons[2].value;
         draw = false;
     } else if (buttons[0].value != " " &&
@@ -118,9 +119,12 @@ function CheckWinner() {
         buttons[8].value != " ") {
         console.log("yaimi");
         draw = true;
+        stats.textContent = "Draw!";
+        canPlay = false;
     }
 
     if (!draw) {
         stats.textContent = "Winner: " + winner;
+        canPlay = false;
     }
 }
